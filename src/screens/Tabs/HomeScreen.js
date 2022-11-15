7c23aff23473d2fdc1c2ea1cb488fa006fbeb4e8
Omer Mert Hayir <mermert251@gmail.com>
2022-11-16 00:17:30 +0300

    design for both tab pages are done also filtering works on both

--- src/screens/Tabs/HomeScreen.js
+++ src/screens/Tabs/HomeScreen.js
@@ -1,24 +1,238 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import listData from 'OneriverApp/data.json';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = () => {
+  const [filterValue, setFilterValue] = useState('All');
+
   return (
     <View style={styles.scaffold}>
-      <Text style={styles.text}>Home Screen</Text>
+      <View style={styles.appBar}>
+        <TouchableOpacity>
+          <Image source={require('OneriverApp/src/images/drawer_icon.png')} />
+        </TouchableOpacity>
+        <Image source={require('OneriverApp/src/images/search_icon.png')} />
+      </View>
+      <View style={styles.titleTextArea}>
+        <Text style={styles.titleTextOneriver}>
+          Oneriver
+          <Text style={styles.titleTextTrade}> Trade</Text>
+        </Text>
+      </View>
+      <View style={styles.filterButtonsArea}>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('All');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'All')}>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('Crypto');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'Crypto')}>Crypto</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('Currency');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'Currency')}>Currency</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.listArea}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={getFilteredList(listData, filterValue)}
+          renderItem={({item}) => (
+            <View style={styles.listItemStyle}>
+              <View style={styles.iconAndNameView}>
+                <Image
+                  style={styles.listIconStyle}
+                  source={getCorrectIcon(item.name)}
+                />
+                <Text style={styles.listItemNameText}>{item.name}</Text>
+              </View>
+              <View style={styles.listItemNumberView}>
+                <Text style={styles.listItemValueText}>
+                  {'$ ' + item.value}
+                </Text>
+                <Text style={checkOverralIncrease(item.rise)}>
+                  {item.percentage}
+                </Text>
+              </View>
+              <TouchableOpacity style={styles.buyButtonStyle}>
+                <Text style={styles.buyButtonTextStyle}>Buy</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
+};
+
+const getFilteredList = (list, value) => {
+  let filteredList = [];
+  for (let index = 0; index < list.length; index++) {
+    if (list[index].type === value || value === 'All') {
+      filteredList.push(list[index]);
+    }
+  }
+  return filteredList;
+};
+
+const getCorrectIcon = name => {
+  switch (name) {
+    case 'BTC':
+      return require('OneriverApp/src/images/crypto-currency/btc_icon.png');
+    case 'ETH':
+      return require('OneriverApp/src/images/crypto-currency/eth_icon.png');
+    case 'BNB':
+      return require('OneriverApp/src/images/crypto-currency/bnb_icon.png');
+    case 'XRP':
+      return require('OneriverApp/src/images/crypto-currency/xrp_icon.png');
+    case 'USDT':
+      return require('OneriverApp/src/images/crypto-currency/usdt_icon.png');
+    case 'TRY':
+      return require('OneriverApp/src/images/crypto-currency/try_icon.png');
+    default:
+      return require('OneriverApp/src/images/crypto-currency/btc_icon.png');
+  }
+};
+
+const checkOverralIncrease = bool => {
+  return bool
+    ? styles.listItemPercentangeTextRise
+    : styles.listItemPercentangeTextDown;
+};
+
+const checkIfSelected = (value, type) => {
+  return value === type ? styles.buttonTextSelected : styles.buttonTextNormal;
 };
 
 const styles = StyleSheet.create({
   scaffold: {
     backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
+  appBar: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 31,
+    paddingHorizontal: 27,
+  },
+  titleTextArea: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingLeft: 27,
+  },
+  titleTextOneriver: {
     color: '#1A4184',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  titleTextTrade: {
+    color: '#1E1E1E',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  filterButtonsArea: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: 30,
+  },
+  filterButtons: {
+    flex: 1,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: '#EBF3FF',
+    marginHorizontal: 10,
+  },
+  buttonTextNormal: {
+    color: '#000000',
+    opacity: 0.63,
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingVertical: 6,
+  },
+  buttonTextSelected: {
+    color: '#1A4184',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingVertical: 6,
+  },
+  listArea: {
+    flex: 12,
+    flexDirection: 'row',
+    marginHorizontal: 30,
+    marginVertical: 20,
+  },
+  listItemStyle: {
+    borderRadius: 10,
+    backgroundColor: '#E7E7E7',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    padding: 12,
+    marginVertical: 8,
+  },
+  listItemNameText: {
+    color: 'black',
     fontSize: 24,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  listIconStyle: {
+    marginEnd: 15,
+  },
+  iconAndNameView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  listItemNumberView: {
+    width: 120,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  listItemValueText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  listItemPercentangeTextRise: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  listItemPercentangeTextDown: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  buyButtonStyle: {
+    borderRadius: 10,
+    backgroundColor: 'rgba(40,222,10,0.2)',
+    marginLeft: 10,
+  },
+  buyButtonTextStyle: {
+    color: '#1DBA04',
+    paddingHorizontal: 12,
+    paddingVertical: 4,
   },
 });
 
--- src/screens/Tabs/WalletScreen.js
+++ src/screens/Tabs/WalletScreen.js
@@ -1,24 +1,229 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
 
 const WalletScreen = () => {
+  const [filterValue, setFilterValue] = useState('All');
+
   return (
     <View style={styles.scaffold}>
-      <Text style={styles.text}>Wallet Screen</Text>
+      <View style={styles.appBar}>
+        <TouchableOpacity>
+          <Image source={require('OneriverApp/src/images/drawer_icon.png')} />
+        </TouchableOpacity>
+        <Image source={require('OneriverApp/src/images/search_icon.png')} />
+      </View>
+      <View style={styles.titleTextArea}>
+        <Text style={styles.titleTextMy}>
+          My
+          <Text style={styles.titleTextWallet}> Wallet</Text>
+        </Text>
+      </View>
+      <View style={styles.filterButtonsArea}>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('All');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'All')}>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('Crypto');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'Crypto')}>Crypto</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.filterButtons}
+          onPress={() => {
+            setFilterValue('Currency');
+          }}>
+          <Text style={checkIfSelected(filterValue, 'Currency')}>Currency</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.listArea}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={getFilteredList(
+            [
+              {
+                name: 'BTC',
+                value: '16,923.68',
+                amount: '3',
+                type: 'Crypto',
+              },
+              {
+                name: 'TRY',
+                value: '0,054',
+                amount: '300',
+                type: 'Currency',
+              },
+            ],
+            filterValue,
+          )}
+          renderItem={({item}) => (
+            <View style={styles.listItemStyle}>
+              <View style={styles.iconAndNameView}>
+                <Image
+                  style={styles.listIconStyle}
+                  source={getCorrectIcon(item.name)}
+                />
+                <Text style={styles.listItemNameText}>{item.name}</Text>
+              </View>
+              <View style={styles.listItemNumberView}>
+                <Text style={styles.listItemAmountText}>{item.amount}</Text>
+                <Text style={styles.listItemValueText}>
+                  {'$ ' + item.value}
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
+};
+
+const getFilteredList = (list, value) => {
+  let filteredList = [];
+  for (let index = 0; index < list.length; index++) {
+    if (list[index].type === value || value === 'All') {
+      filteredList.push(list[index]);
+    }
+  }
+  return filteredList;
+};
+
+const getCorrectIcon = name => {
+  switch (name) {
+    case 'BTC':
+      return require('OneriverApp/src/images/crypto-currency/btc_icon.png');
+    case 'ETH':
+      return require('OneriverApp/src/images/crypto-currency/eth_icon.png');
+    case 'BNB':
+      return require('OneriverApp/src/images/crypto-currency/bnb_icon.png');
+    case 'XRP':
+      return require('OneriverApp/src/images/crypto-currency/xrp_icon.png');
+    case 'USDT':
+      return require('OneriverApp/src/images/crypto-currency/usdt_icon.png');
+    case 'TRY':
+      return require('OneriverApp/src/images/crypto-currency/try_icon.png');
+    default:
+      return require('OneriverApp/src/images/crypto-currency/btc_icon.png');
+  }
+};
+
+const checkIfSelected = (value, type) => {
+  return value === type ? styles.buttonTextSelected : styles.buttonTextNormal;
 };
 
 const styles = StyleSheet.create({
   scaffold: {
     backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
+  appBar: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 31,
+    paddingHorizontal: 27,
+  },
+  titleTextArea: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingLeft: 27,
+  },
+  titleTextMy: {
     color: '#1A4184',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  titleTextWallet: {
+    color: '#1E1E1E',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  filterButtonsArea: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    paddingTop: 30,
+  },
+  filterButtons: {
+    flex: 1,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: '#EBF3FF',
+    marginHorizontal: 10,
+  },
+  buttonTextNormal: {
+    color: '#000000',
+    opacity: 0.63,
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingVertical: 6,
+  },
+  buttonTextSelected: {
+    color: '#1A4184',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingVertical: 6,
+  },
+  listArea: {
+    flex: 12,
+    flexDirection: 'row',
+    marginHorizontal: 30,
+    marginVertical: 20,
+  },
+  listItemStyle: {
+    borderRadius: 10,
+    backgroundColor: '#E7E7E7',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 12,
+    marginVertical: 8,
+  },
+  listItemNameText: {
+    color: 'black',
     fontSize: 24,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  listIconStyle: {
+    marginEnd: 15,
+  },
+  iconAndNameView: {
+    width: 120,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-start',
+  },
+  listItemNumberView: {
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  listItemAmountText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  listItemValueText: {
+    color: '#1E1E1E',
+    fontSize: 20,
+    fontWeight: 'bold',
+    opacity: 0.7,
   },
 });
 
